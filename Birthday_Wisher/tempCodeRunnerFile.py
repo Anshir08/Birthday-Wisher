1b80c4575016b42d74351f5a1f0b1cf1b4711fd5
Anshir <69457505+Anshir08@users.noreply.github.com>
2021-09-03 10:21:32 -0700

    Delete tempCodeRunnerFile.py

--- Birthday_Wisher/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-for index, item in df.iterrows():
-    #     # print(index, item['Birthday'])
-    #     bday = item['Birthday'].strftime("%d-%m")
-
-    #     if (today == bday):
-    #         sendEmail(item['Email'], "Happy Birthday", item['Dialogue'])
